5f892dccbf876ddcc5d0ad0b9d8843fafdb9c56c
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-21 09:51:37 +0300

    fix

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1>It's working on host2media</h1>
-    <p>nadim</p>
-    <p>this is a test on cpanel</p>
+    <h1>Netlify Done Deploy with Actions</h1>
   </div>
 </template>
