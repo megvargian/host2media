59bfe6bcb326fcef2ba04d8f1e577c9def5a0e9b
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-08-23 15:46:18 +0300

    add header on desktop

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
-  <div>
-    <h1>Netlify Done Deploy with Actions</h1>
-  </div>
+  <Header />
 </template>
---
+++ components/Header.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="w-[100vw]">
+        <header>
+            <nav>
+                <div class="w-full py-3 bg-[#5564ad]">
+                    <ul class="flex justify-end text-white px-5">
+                        <li class="mx-2">
+                            <a href="#">
+                                Blog
+                            </a>
+                        </li>
+                        <li class="mx-2">
+                            <a href="#">
+                                Knowladge Base
+                            </a>
+                        </li>
+                        <li class="mx-2">
+                            <a href="#">
+                                Status
+                            </a>
+                        </li>
+                        <li class="mx-2">
+                            <a href="#">
+                                Customer Portal
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <nav class="py-5 flex flex-row justify-start lg:pl-64 md:pl-16 sm:pl-8">
+                <ul class="basic-2/4 flex bg-white font-bold ">
+                    <li class="mx-2">
+                        <a href="#">
+                            Website Hosting
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            Domains
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            VPS
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            Professional Email
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            SSL Cerificates
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            Reseller Program
+                        </a>
+                    </li>
+                    <li class="mx-2">
+                        <a href="#">
+                            Dev Serices
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+</template>
