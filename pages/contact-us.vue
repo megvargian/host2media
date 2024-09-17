db328171ff487a9c7cfe2cccd18a97c027f25c90
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2024-09-17 17:35:41 +0300

    add section contact us

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,16 +1,182 @@
 <template>
-    <section class="pt-[42px]">
-        <div class="w-full z-1 relative">
-        <img
-          class="w-full -z-1"
-          src="~/assets/images/contact-us-main.png"
-          alt="banner"
-        />
-        <h1
-          class="leading-tight sm:text-4xl md:lg:text-5xl md:text-4xl sm:text-[36px] text-center text-[36px] text-white font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-        >
-            Contact Us, <br> We’re Just a Click Away!
-        </h1>
+  <section class="pt-[42px]">
+    <div class="w-full z-1 relative">
+      <img
+        class="w-full -z-1"
+        src="~/assets/images/contact-us-main.png"
+        alt="banner"
+      />
+      <h1
+        class="leading-tight sm:text-4xl md:lg:text-5xl md:text-4xl sm:text-[36px] text-center text-[36px] text-white font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        Contact Us, <br />
+        We’re Just a Click Away!
+      </h1>
+      <div
+        class="container w-full mx-auto lg:absolute lg:right-0 lg:left-0 bottom-[-50%]"
+      >
+        <div class="grid grid-cols-12 lg:gap-10 gap-5 py-10 lg:mx-0">
+          <div class="lg:col-span-4 col-span-6 bg-white sm:p-5 p-3 rounded-md !pb-16">
+            <div class="flex justify-center items-center pr-32 sm:pb-10 pb-5">
+              <img
+                class="md:w-12 md:h-12 w-8 h-8"
+                src="~/assets/icons/ask.png"
+                alt="ask"
+              />
+            </div>
+            <p class="text-[#0F132A] text-lg Mulish-Black font-black pb-4">
+                Knowledge Base
+            </p>
+            <p class="text-[#0F132A]">Step-by-step guides. Frequently Asked <br class="sm:flex hidden" /> Questions. Self-help at your fingertips.</p>
+          </div>
+          <div class="lg:col-span-4 col-span-6 bg-white sm:p-5 p-3 rounded-md !pb-16">
+            <div class="flex justify-center items-center pr-32 sm:pb-10 pb-5">
+              <img
+                class="md:w-12 md:h-12 w-8 h-8"
+                src="~/assets/icons/status.png"
+                alt="status"
+              />
+            </div>
+            <p class="text-[#0F132A] text-lg Mulish-Black font-black pb-4">
+                Service Status
+            </p>
+            <p class="text-[#0F132A]">Current operational status of all <br class="sm:flex hidden" /> Host2Media Services.</p>
+          </div>
+          <div class="lg:col-span-4 col-span-6 bg-white sm:p-5 p-3 rounded-md !pb-16">
+            <div class="flex justify-center items-center pr-32 sm:pb-10 pb-5">
+              <img
+                class="md:w-12 md:h-12 w-8 h-8"
+                src="~/assets/icons/blog.png"
+                alt="blog"
+              />
+            </div>
+            <p class="text-[#0F132A] text-lg Mulish-Black font-black pb-4">
+                Blog
+            </p>
+            <p class="text-[#0F132A]">Step-by-step guides. Frequently Asked <br class="sm:flex hidden" /> Questions. Self-help at your fingertips.</p>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
+  </section>
+  <section class="pt-64 pb-10">
+    <div class="container mx-auto flex justify-center items-center pb-6">
+        <h2 class="leading-tight lg:text-5xl md:text-4xl sm:text-[36px] text-[25px] lg:text-left text-center text-[#0F132A] font-black pb-5">
+            Around-The-Clock <br class="sm:flex hidden" /> In-House Support
+        </h2>
+    </div>
+    <div class="container mx-auto flex justify-center itmes-center">
+      <p class="text-center sm:mx-0 mx-4">
+        Customers trust Host2Media as their trusted online web hosting solution,<br class="sm:flex hidden" />  our support team of in-house hosting professionals is accessible round the clock. <br class="sm:flex hidden" />With the support of Host2Media technical team representatives, support is always just a quick message away<br class="sm:flex hidden" /> via email, web chat or WhatsApp
+      </p>
+    </div>
+  </section>
+  <section class="relative">
+    <div class="bg-[#5564AD] py-10">
+      <div class="container w-full mx-auto">
+        <div class="grid grid-cols-12 gap-4 py-10">
+          <div class="lg:col-span-6 col-span-12 flex lg:justify-start justify-center lg:pl-10">
+            <h2 class="leading-tight lg:text-5xl md:text-4xl sm:text-[36px] text-[25px] lg:text-left text-center font-bold text-white">
+              An Online Solution <br class="sm:flex hidden" />
+              You can Trust
+            </h2>
+          </div>
+          <div class="lg:col-span-6 col-span-12 order-2 flex justify-center items-center mx-5 lg:pb-0 pb-32">
+            <div class="w-full">
+              <p class="text-white">
+                Our Technical Support team is fully empowered to collaborate effectively in reaching your online goals. Whether you need assistance with your hosting solutions or guidance for enhancing your website,  we have the expertise to assist you. Our support team members are equipped to address a diverse range of challenges.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="relative">
+    <div
+      class="custom-container mx-auto z-2 bg-white rounded-[50px] mx-10"
+    >
+      <div class="px-10 flex justify-center items-center">
+        <div class="rounded-[50px] pt-10">
+          <div class="grid grid-cols-12 lg:gap-10 gap-5 flex">
+            <div class="lg:col-span-6 col-span-12 lg:pr-[10rem] flex justify-center items-center">
+              <div>
+                <h2
+                  class="leading-tight lg:text-5xl md:text-4xl sm:text-[36px] text-[25px] lg:text-left text-center text-[#0F132A] font-black"
+                >
+                  Host2Media <br class="sm:flex hidden" />
+                  contributes to <br class="sm:flex hidden" />
+                  your success.
+                </h2>
+                <p class="lg:py-12 py-8 font-normal text-lg lg:text-left text-center">
+                  Customers are choosing to migrate their services to Host2Media
+                  to benefit from our fast hosting and top-notch support.
+                </p>
+                <div class="flex justify-center items-center">
+                  <button
+                    class="rounded-[10px] bg-[#5564AD] text-white text-sm font-bold py-4 px-8 Mulish-ExtraBold"
+                  >
+                    Talk To Sales For Hosting Offers
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="lg:col-span-6 col-span-12">
+              <div class="text-left pb-6 flex lg:block justify-center">
+               <div>
+                  <p class="text-[#0F132A] text-lg Mulish-Black font-black">
+                    Easy Control Panel
+                  </p>
+                  <p class="font-light Mulish-light pt-1">
+                    We employ a user-friendly hosting <br class="sm:flex hidden" />
+                    control panel, simplifying the <br class="sm:flex hidden" />
+                    management of your web hosting and <br class="sm:flex hidden" />
+                    alleviating any associated challenges.
+                  </p>
+               </div>
+              </div>
+              <div class="text-left pb-6 lg:block flex justify-center">
+                <div>
+                  <p class="text-[#0F132A] text-lg Mulish-Black font-black">
+                    Comprehensive in functionality.
+                  </p>
+                  <p class="font-light Mulish-light pt-1">
+                    We go beyond web hosting, providing <br class="sm:flex hidden" />
+                    a tool suite that Incorporates all the <br class="sm:flex hidden" />
+                    essential elements for your success.
+                  </p>
+                </div>
+              </div>
+              <div class="text-left pb-6 lg:block flex justify-center">
+                <div>
+                  <p class="text-[#0F132A] text-lg Mulish-Black font-black">
+                    Fast & Reliable
+                  </p>
+                  <p class="font-light Mulish-light pt-1">
+                    This leads to improved SEO rankings, <br class="sm:flex hidden" />
+                    reduced bounce rates, and increased <br class="sm:flex hidden" />
+                    conversion rates!
+                  </p>
+                </div>
+              </div>
+              <div class="text-left pb-6 lg:block flex justify-center">
+                <div>
+                  <p class="text-[#0F132A] text-lg Mulish-Black font-black">
+                    7/7 Support
+                  </p>
+                  <p class="font-light Mulish-light pt-1">
+                    Reach out to our customer service <br class="sm:flex hidden" />
+                    team at any time. Our dedicated <br class="sm:flex hidden" />
+                    support team is ever-ready to address <br class="sm:flex hidden" />
+                    and resolve any questions or concerns <br class="sm:flex hidden" />
+                    you may have.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
