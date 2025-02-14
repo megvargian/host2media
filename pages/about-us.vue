8086f072b25098aed89391d8c2844ba7e8013a3d
mikekouyoumdjian <44778573+megvargian@users.noreply.github.com>
2025-02-14 14:57:02 +0200

    add about us page

---
+++ pages/about-us.vue
@@ -0,0 +1,214 @@
+<template>
+  <section class="relative mb-32">
+    <div
+      class="custom-container mx-auto bg-white rounded-[50px] mx-10 relative"
+    >
+      <div class="px-16 flex justify-start items-center">
+        <div class="py-16">
+          <div class="grid grid-cols-12 lg:gap-16 gap-5">
+            <div class="col-span-12">
+              <h1
+                class="text-base text-left text-[#0F132A] font-black mb-4 Mulish-Black"
+              >
+                About Us
+              </h1>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                Welcome to Host2Media, where innovation meets reliability in web
+                hosting and digital solutions. <br />
+                Since 2010, we have been empowering businesses of all sizes to
+                thrive in the ever-evolving digital landscape.
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                At Host2Media, we understand that your online presence is more
+                than just a website—it’s the heart of your brand and a vital
+                connection to your audience. That’s why we are dedicated to
+                delivering exceptional web hosting, domain name registration,
+                and professional email services tailored to your needs. With a
+                focus on speed, security, and personalized support, we are your
+                trusted advocate in building a successful online presence.
+              </p>
+              <p class="text-base text-left text-[#0F132A] font-bold mb-4">
+                Our Mission
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                To provide secure, scalable, and SEO-friendly web hosting
+                services while ensuring a seamless and personalized experience
+                for our clients. <br />
+                We strive to be more than a service provider—we aim to be your
+                trusted ally in success.
+              </p>
+              <h2
+                class="text-base text-left text-[#0F132A] font-black mb-1 Mulish-Black"
+              >
+                What Makes Us Different
+              </h2>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Reliability
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                Reliability is at the core of everything we do. <br />
+                We ensure your website remains online, secure, and accessible at
+                all times. <br />
+                With robust infrastructure, state-of-the-art servers, and a
+                dedicated support team, we guarantee a hosting experience you
+                can count on.
+              </p>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Empowerment
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                We empower businesses with the tools and solutions needed to
+                succeed in the digital age <br />
+                From customizable hosting packages to expert guidance, we
+                provide resources to help you take control of your online
+                presence with confidence.
+              </p>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Customer Commitment
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                Our clients are at the heart of everything we do. We are
+                committed to delivering exceptional customer service by
+                listening to your needs, offering tailored solutions, and going
+                the extra mile to ensure your satisfaction. Your success is our
+                priority, and we are here to support you every step of the way.
+              </p>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                What We Offer
+              </p>
+              <ul class="list-disc pl-5">
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Reliable Web Hosting: High-performance hosting solutions
+                    designed for speed, security, and reliability, ensuring your
+                    website ranks well in search results.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Professional Email: Custom email solutions to boost your
+                    brand’s credibility and enhance client communication.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Domain Name Registration: Secure and manage your domain
+                    names easily, helping your business stand out online.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Custom Solutions: From WordPress optimization to bespoke
+                    hosting configurations, we’ve got you covered.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Malware Protection & Recovery: Protect your website with our
+                    advanced malware detection, removal, and recovery services.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    24/7 Support: Our dedicated team ensures your business stays
+                    online, resolving issues promptly.
+                  </p>
+                </li>
+              </ul>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Why Choose Host2Media
+              </p>
+              <ul class="list-decimal pl-5 mb-4">
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Proven Experience: Over a decade of expertise in the web
+                    hosting industry, building a reputation for reliable and
+                    professional services.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    SEO Optimization Focus: Our services enhance your website’s
+                    visibility on search engines, driving more traffic to your
+                    business.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Customer-Centric Approach: Your success is our priority, and
+                    we’re here to support you every step of the way.
+                  </p>
+                </li>
+                <li class="text-base text-left text-[#0F132A] mb-4">
+                  <p>
+                    Cutting-Edge Technology: Leveraging the latest tools and
+                    innovations to keep your website ahead of the competition.
+                  </p>
+                </li>
+              </ul>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Our Vision
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-16">
+                To create a world where businesses and individuals unlock their
+                full potential by leveraging SEO-friendly, secure, and
+                high-performing digital solutions. <br />
+                We aspire to be a global leader in web hosting, recognized for
+                our integrity, innovation, and commitment to client success.
+              </p>
+              <h2
+                class="text-base text-left text-[#0F132A] font-black mb-1 Mulish-Black"
+              >
+                Meet the Founder
+              </h2>
+              <p class="text-base text-left text-[#0F132A] font-bold">
+                Nadim Younes
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                Founder | Business Strategist
+              </p>
+              <p class="text-base text-left text-[#0F132A] mb-4">
+                Nadim YounesFounder | Business StrategistFounded in 2010,
+                Host2Media is the vision of Nadim Younes—a Business Strategist
+                shaping the future of web hosting. <br />With over a decade of
+                expertise, Nadim has built Host2Media into a trusted name for
+                secure, high-performance, and innovative hosting solutions.
+                <br />
+                His strategic mindset and hands-on approach ensure businesses
+                don’t just get hosting—they get a powerful foundation for
+                growth. <br />
+                Fueled by a passion for technology and an unwavering commitment
+                to excellence, Nadim leads Host2Media with integrity,
+                continuously redefining industry standards.
+              </p>
+              <h2
+                class="text-base text-left text-[#0F132A] font-black mb-1 Mulish-Black"
+              >
+                A Message from the Founder
+              </h2>
+              <i class="text-base text-left text-[#0F132A]">
+                <p class="mb-4">
+                  “The digital world is built on one thing—trust. And trust is
+                  earned through reliability, security, and performance. At
+                  Host2Media, we don’t just offer hosting; we engineer solutions
+                  that empower businesses to thrive. From fully managed hosting
+                  to secure domain registration and expert support, every
+                  service we provide is designed for one purpose: to give you
+                  the foundation to grow, scale, and succeed—without limits.
+                  Success isn’t about chance; it’s about strategy, resilience,
+                  and making the right choices. If you’re ready to elevate your
+                  digital presence, I personally welcome you to Host2Media.
+                  Let’s build something that lasts.”
+                </p>
+              </i>
+              <p class="text-base text-left text-[#0F132A]">— Nadim Younes</p>
+            </div>
+          </div>
+          <div class="contact-us-form"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
